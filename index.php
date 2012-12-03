772770ac349f0e741cd0ab489f46429ccea14955
jchang-bcp <jonathan.chang13@gmail.com>
2012-12-03 12:15:04 -0800

    Dis iz my kind of CSS Profile XD

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Justice Summit Signup System Code Name "Batman"</title>
+	<link rel="stylesheet" type="text/css" href="BCP.css" />
 </head>
 
 <body>
+	<div class="header">
+		Justice Summit 2013
+	</div>
+	<div class="main">
+		<form action="PLACEHOLDER.php" method="post">
+			<input type="submit" class="submitbutton" name="submit" value="Test Button" />
+		</form>
+	</div>
 </body>
 </html>
