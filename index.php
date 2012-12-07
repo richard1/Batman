44808b61c46875bea296fbaaa649c8b7f2fdcc3a
jchang-bcp <jonathan.chang13@gmail.com>
2012-12-07 09:42:42 -0800

    Moar CSS

--- index.php
+++ index.php
@@ -8,10 +8,23 @@
 
 <body>
 	<div class="header">
-		Justice Summit 2013
+		<table width=100%>
+		<tr><td>Justice Summit</td>
+		<td><img src="bell.png" height=100px width=100px align=right /></td></tr>
+		</table>
 	</div>
+    <br />
 	<div class="main">
-		<form action="PLACEHOLDER.php" method="post">
+		<form action="viewSessions.php" method="post">
+        	<div id="mainselection">
+			     <select>
+			     <option>Select an Option</option>
+			     <option>Option 1</option>
+			     <option>Option 2</option>
+			     </select>
+			</div>
+            <br />
+            <input type="hidden" name="id" value="TEST" />
 			<input type="submit" class="submitbutton" name="submit" value="Test Button" />
 		</form>
 	</div>
--- viewSessions.php
+++ viewSessions.php
@@ -9,19 +9,21 @@
 <body>
 	<?php
 		require("conf.inc.php");
-        if(isset($_GET['id'])) {
-            $id = $_GET['id'];
+        if(isset($_POST['id'])) {
+            $id = $_POST['id'];
 			echo "<h2>Welcome, $id</h2>";
 		}
 		
-		$mysqli = new mysqli($CFG->localhost, $CFG->username, $CFG->password, $CFG->database);
+		echo $CFG->user_name;
+		
+		$mysqli = new mysqli($CFG->server, $CFG->user_name, $CFG->password, $CFG->database);
 		if ($mysqli->connect_errno) {
 			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		}
 		
 		$query = $mysqli->query("SELECT sessionID FROM user_sessions WHERE userID = $id") or die("Failed to lookup session ID");
 		while ($row = $query->fetch_assoc()) {
-			$query2 = $mysqli->query("SELECT name FROM sessions WHERE userID = $row['sessionID']") or die("Failed to lookup session name");
+			$query2 = $mysqli->query("SELECT name FROM sessions WHERE userID = {$row['sessionID']}") or die("Failed to lookup session name");
 			while ($row2 = $query2->fetch_assoc()) {
 				echo " name = " . $row2['name'] . "\n";
 			}
